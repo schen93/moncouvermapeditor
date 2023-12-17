11f5387c570b966bb831d3af0832b5beabd22a93
schen93 <chenshuo93@hotmail.com>
2023-12-16 23:52:59 -0800

    schen93h rebase continue

--- src/com/cs93/Util.java
+++ src/com/cs93/Util.java
@@ -3,6 +3,6 @@ package com.cs93;
 public class Util {
 
     public static int getRandomInt(int min, int max) {
-        return 0;//(int)Math.round(Math.random() * ( max - min )) + min;
+        return Integer.valueOf("h");//(int)Math.round(Math.random() * ( max - min )) + min;
     }
 }
