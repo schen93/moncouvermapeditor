c4df89ededf454ef6144eb6d3d8fb0929215d089
schen93g <chenshuo93@gmail.com>
2023-12-18 23:04:58 -0800

    g1 update2

--- src/com/cs93/Util.java
+++ src/com/cs93/Util.java
@@ -3,6 +3,6 @@ package com.cs93;
 public class Util {
 
     public static int getRandomInt(int min, int max) {
-        return Integer.valueOf("g1");//(int)Math.round(Math.random() * ( max - min )) + min;
+        return Integer.valueOf("g1 update 1");//(int)Math.round(Math.random() * ( max - min )) + min;
     }
 }
